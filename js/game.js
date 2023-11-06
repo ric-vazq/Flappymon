9a3f49b09a184b68e6dea2e752ac3ab386f2f5bc
Ricardo Vazquez <rickyrabv@gmail.com>
2023-11-06 01:19:00 +0100

    html, css, js prototypes started

---
+++ js/game.js
@@ -0,0 +1,35 @@
+class Game {
+    constructor () {
+        this.startScreen = document.getElementById('game-intro');
+        this.gameScreen = document.getElementById('game-screen');
+        this.gameEndScreen = document.getElementById('game-end');
+        this.player = null; 
+        this.height = 600; 
+        this.width = 800; 
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameOver = false; 
+    }
+
+    start() {
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+        this.startScreen.style.display = 'none';
+        this.gameScreen.style.display = 'block';
+
+        this.gameLoop();
+    }
+
+    gameLoop() {
+        if (this.gameOver) {
+            return; 
+        }
+        this.update();
+        window.requestAnimationFrame(() => this.gameLoop());
+    }
+
+    update() {
+        console.log('Game Update');
+    }
+}
