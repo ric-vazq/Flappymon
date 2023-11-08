3647b524198a7beb50a9af8c1ac610d92ea03b20
Ricardo Vazquez <rickyrabv@gmail.com>
2023-11-08 16:26:53 +0100

    start screen css html complete

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@ class Game {
         this.startScreen = document.getElementById('game-intro');
         this.gameScreen = document.getElementById('game-screen');
         this.gameEndScreen = document.getElementById('game-end');
+        this.gameStats = document.getElementById('stats-container')
         this.player = null; 
         this.height = 600; 
         this.width = 800; 
@@ -16,6 +17,7 @@ class Game {
         this.gameScreen.style.height = `${this.height}px`;
         this.gameScreen.style.width = `${this.width}px`;
         this.startScreen.style.display = 'none';
+        this.gameScreen.style.display = 'block';
         this.gameScreen.style.display = 'block';
 
         this.gameLoop();
