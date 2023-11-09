4d872bf0f943a42a7546b16b7fd459b26cb5eafd
Ricardo Vazquez <rickyrabv@gmail.com>
2023-11-09 18:53:07 +0100

    misc changes

--- js/game.js
+++ js/game.js
@@ -1,15 +1,14 @@
 class Game {
     constructor () {
         this.startScreen = document.getElementById('game-intro');
-        this.gameScreen = document.getElementById('game-screen');
+        this.gameScreen = document.getElementById('game-container')
         this.gameEndScreen = document.getElementById('game-end');
-        this.gameStats = document.getElementById('stats-container')
         this.player = null; 
         this.height = 600; 
         this.width = 800; 
         this.obstacles = [];
         this.score = 0;
-        this.lives = 3;
+        this.lives = 1;
         this.gameOver = false; 
     }
 
@@ -17,8 +16,7 @@ class Game {
         this.gameScreen.style.height = `${this.height}px`;
         this.gameScreen.style.width = `${this.width}px`;
         this.startScreen.style.display = 'none';
-        this.gameScreen.style.display = 'block';
-        this.gameStats.style.display = 'block';
+        this.gameScreen.style.display = 'flex';
 
         this.gameLoop();
     }
