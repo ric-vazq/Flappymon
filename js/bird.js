07bc63fe2c10040fbc0c39ce8b1d5fb15c223660
Vitor193 <vitormiguel135@gmail.com>
2023-11-09 17:51:11 +0000

    done

--- js/bird.js
+++ js/bird.js
@@ -1,1 +1,14 @@
-class Bird {}
+class Bird {
+    constructor(gameScreen , left , top , width , height , imgSrc){
+        this.gameScreen = gameScreen;
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.directionX = 0; 
+        this.directionY = 0;
+        this.element = document.createElement('img');
+        this.element.src = imgSrc;
+
+    }
+}
